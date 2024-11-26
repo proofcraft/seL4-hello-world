e982533d347d0a071018c6942054fa3ad98a74da
Indan Zupancic <indan@nul.nu>
2024-11-26 12:53:25 +0000

    imx93: Update Remotes
    Also improve hello.
    
    Signed-off-by: Indan Zupancic <indan@nul.nu>

--- projects/hello/src/hello.c
+++ projects/hello/src/hello.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <sel4/sel4.h>
+#include <sel4runtime.h>
 
 void main(void)
 {
     printf("Hello World!\n");
 
+    const seL4_BootInfo* b = sel4runtime_bootinfo();
+    int n = b->untyped.end - b->untyped.start;
+
+    printf("initThreadCNodeSizeBits = %lu\n", b->initThreadCNodeSizeBits);
+    printf("Printing out %d UTs\n", n);
+    for (int i = 0; i < n; ++i) {
+        struct seL4_UntypedDesc u = b->untypedList[i];
+
+        printf("%d: %s %d\t 0x%lx\n", i, u.isDevice ? "dev" : "mem", u.sizeBits, u.paddr);
+    }
+    printf("Done.\n");
     /* There is nothing to return to: */
     while(1);
 }
